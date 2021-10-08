79f1b24e273ce04f09703f202eb8aef4e283f609
OsaeAddo <immanueloaddo@gmail.com>
2021-10-08 09:34:02 +0000

    modify test

--- users/tests.py
+++ users/tests.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 
 class CustomUserTests(TestCase):
+    
     def test_create_user(self):
         User = get_user_model()
         user = User.objects.create_user(
