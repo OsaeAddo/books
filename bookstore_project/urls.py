722f3231096ff20fb0c8064911ab73145c3e61c1
OsaeAddo <immanueloaddo@gmail.com>
2021-10-10 23:12:31 +0000

    add url path for account registration and signup

--- bookstore_project/urls.py
+++ bookstore_project/urls.py
@@ -18,5 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('pages.urls')),
+    
+    # User management
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
