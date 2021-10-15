f4362dd4ac383ef69a9aeb35d039f1e14a9a4b6b
OsaeAddo <immanueloaddo@gmail.com>
2021-10-15 22:19:30 +0000

    add SignupPageView

--- users/views.py
+++ users/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+class SignupPageView(generic.CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'signup.html'
